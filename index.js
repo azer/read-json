bd55ce727d14e32db2ad839aba1e9acef51e47c7
E.Azer Koçulu <azer@kodfabrik.com>
2015-10-02 10:02:25 -0700

    use try-call for optimization

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 var fs = require("fs");
+var call = require("try-call");
 
 module.exports = readJSON;
 
 function readJSON(filename, options, callback){
-  if(callback === undefined){
+  if(arguments.length == 2){
     callback = options;
     options = {};
   }
 
   fs.readFile(filename, options, function(error, bf){
     if(error) return callback(error);
-
-    try {
-      bf = JSON.parse(bf.toString().replace(/^\ufeff/g, ''));
-    } catch (err) {
-      callback(err);
-      return;
-    }
-
-    callback(undefined, bf);
+    call(parse.bind(null, bf), callback);
   });
+
+  function parse (bf) {
+    return JSON.parse(bf.toString().replace(/^\ufeff/g, ''));
+  }
+
 }
--- test.js
+++ test.js
@@ -1,16 +1,40 @@
 var test = require('prova');
-var readJSON = require("./");
+var fs = require("fs");
+var read = require("./");
 
 test('reads a JSON file', function (t) {
-  readJSON('./package.json', function(error, manifest){
+  t.plan(2);
+
+  read('./package.json', function(error, manifest){
+    t.error(error);
     t.equal(manifest.name, 'read-json');
-    t.end();
   });
 });
 
 test('supports fs.readFile options', function (t) {
-  readJSON('./package.json', {encoding: 'utf-8'}, function(error, manifest){
+  t.plan(2);
+
+  read('./package.json', {encoding: 'utf-8'}, function(error, manifest){
+    t.error(error);
     t.equal(manifest.name, 'read-json');
-    t.end();
+  });
+});
+
+test('fails when file is missing', function (t) {
+  t.plan(2);
+
+  read('./missing.json', function(error, doc){
+    t.ok(error);
+    t.notOk(doc);
+  });
+});
+
+test('fails when JSON is invalid', function (t) {
+  var invalid = fs.writeFileSync('/tmp/invalid.json', '}}}');
+
+  t.plan(2);
+  read('/tmp/invalid.json', function (error, doc) {
+    t.ok(error);
+    t.notOk(doc);
   });
 });
